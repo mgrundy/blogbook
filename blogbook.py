b364e29e4f4539cb377fd3b4467abc6aa4bfc580
Michael Grundy <mhgrundy@gmail.com>
2017-01-23 07:18:40 -0500

    Articles are now saved (and unicode errors are vanquished)
    Also, started making things more generic

--- blogbook.py
+++ blogbook.py
@@ -12,12 +12,15 @@ def parse_command_line():
     parser = optparse.OptionParser(usage="Usage: %prog [options]")
 
     parser.add_option("-r", dest="regex", type="string",
-                      help="Build variant regex. Overriden by -a")
+                      help="regex")
+    parser.add_option("-s", dest="search_string", type="string",
+                      help="String to search for that will find page content container")
     parser.add_option("-u", dest="url", type="string",
-                      help="Summarize all variants for the commit")
+                      help="URL to pull content from")
 
     parser.set_defaults(
                         regex=None,
+                        search_string=None,
                         url=None)
 
     return parser.parse_args()
@@ -25,28 +28,28 @@ def parse_command_line():
 
 def get_url(url):
     while url is not None:
-        print url
         response = requests.get(url)
+        #return response.text.encode('utf-8').decode('ascii', 'ignore')
         return response.text
 
 
 
 
-def save_chapters_from_doc(html_doc):
+def save_chapters_from_doc(html_doc, search_string):
 
     soup = BeautifulSoup(html_doc, 'html.parser')
-    # This is how I got to the interesting bits for Prof. Lemeir's blog
-    main = soup.main
-    for m in main.children:
+    #oh! This might work better, and maybe easier to make generic
+    page_articles = soup.find_all(search_string)
+    for m in page_articles:
         if 'id' not in m.attrs:
-            print m.attrs
+            print(m.attrs)
             continue
         # This is the document id we'll save it as
         id = m['id']
+        # This is how I got to the interesting bits for Prof. Lemire's blog
         # This is the title from the header link
         title = m.header.h2.a.string.encode('utf-8')
         #title = m.header.h2.a.string
-        print title
         # This is the string representation of the publish date
         timestamp = m.footer.span.a.time.string.encode('utf-8')
         # This is the contents of the post
@@ -78,8 +81,9 @@ def write_html_file(id, title, timestamp, contents):
     html.append(body)
     body.append(h2)
     body.append(content_span)
-    print "1234567890123456789012345678901234567890123456789012345678901233456789012345678901234567890", id
-    print bs.prettify()
+    print( unicode(bs))
+    with open(id + ".html", "wb") as fh:
+        fh.write(bs.prettify("utf-8"))
 
 
 ## Add a target directory option (example: images/)
@@ -87,7 +91,7 @@ def download_file(file_url):
     filename = file_url.split("/")[-1]
     response = requests.get(file_url, stream=True)
     with open(filename, "wb") as fh:
-        print "Downloading", filename
+        print("Downloading", filename)
         size = 16 * 1024
         for block in response.iter_content(chunk_size=size):
             if block:
@@ -97,10 +101,8 @@ def download_file(file_url):
 def main():
     options, args = parse_command_line()
 
-    url='http://lemire.me/blog/page/158/'
-
-    html_doc = get_url(url)
-    save_chapters_from_doc(html_doc)
+    html_doc = get_url(options.url)
+    save_chapters_from_doc(html_doc, options.search_string)
 
 
 if __name__ == "__main__":
